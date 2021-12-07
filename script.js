eadc52aa44b74935782426043e98299214113c69
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-07 00:01:25 -0800

    Update to Index and structure of const

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const openWeatherEnd = "https://api.openweathermap.org/data/2.5/";
+const openWeatherCurrent = "weather?q=";
+const openWeather1 = "onecall?lat=";
+const openWeather2 = "&lon=";
+const openWeather3 = "&units=imperial";
 
+const savedSearchesName = "weatherDashboardSavedCities"; // localstorage
+const lastSearchName = "weatherDashboardLastViewed";
+var savedSearches = [];
+var lastSearchIndex = -1; 
 //section for city api
 function city(){
 
