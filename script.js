6b6d4b2f4ca04a367fd1cc2f3f9d02d404aa80dd
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-07 00:32:43 -0800

    Update to CitySaved

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ const openWeatherCurrent = "weather?q=";
 const openWeather1 = "onecall?lat=";
 const openWeather2 = "&lon=";
 const openWeather3 = "&units=imperial";
+const APIkey =
 
 const savedSearchesName = "weatherDashboardSavedCities"; // localstorage
 const lastSearchName = "weatherDashboardLastViewed";
-var savedSearches = [];
-var lastSearchIndex = -1; 
-//section for city api
-function city(){
+var savedSearch = [];
+var lastSearch = -1; 
 
+citySaved();
+$(#searchbtn).click(doSearch);
+//section for city api
+function citySaved(){
+    var searchList = JSON.parse(localStorage.getItem(savedSearchesName));
+    lastSearchIndex = localStorage.getItem(lastSearchName);
+    
+        // If valid list returned, set global variable to it
+        if (searchList) {
+            savedSearches = searchList;
+            displaySavedSearches(); // Load displayed city list
+            displayWeatherData(); // Display last viewed city
+        }
+    }
 }
 
 //section for 5 day forcast
