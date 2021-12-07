252085a1968529bbd1b525c58a2208cd9ff1c4b4
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-07 01:11:38 -0800

    Update to CityInfo, SearchDisplay, DoSearch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const openWeatherCurrent = "weather?q=";
 const openWeather1 = "onecall?lat=";
 const openWeather2 = "&lon=";
 const openWeather3 = "&units=imperial";
-const APIkey =
+const APIkey = "&appid=cf842424f641e0b265e27e57cf71442b";
 
 const savedSearchesName = "weatherDashboardSavedCities"; // localstorage
 const lastSearchName = "weatherDashboardLastViewed";
@@ -23,13 +23,6 @@ function citySaved(){
     }
 }
 
-function getCityWeather(event){
-    event.preventDefault();
-    lastSearch = $(this).attr("value");
-    localStorage.setItem(lastSearchName, lastSearch);
-    weatherDisplay();
-}
-
 //Save city
 function setSavedSearch(){
     if (savedSearch.length .0) {
@@ -39,11 +32,56 @@ function setSavedSearch(){
 }
 //display search
 function searchDisplay(){
+    const listItemDef1 = '<a href="#" class="list-group-item list-group-item-action" value=';
+    const listItemDef2 = '>';
+    const listItemDef3 = '</a>'
+    var cityListDiv = $("#city-list"); 
+    cityListDiv.empty();
 
+    for (let i=0; i<savedSearch.length; i++) {
+        cityListDiv.append(listItemDef1 + i + listItemDef2 + savedSearch[i].name + listItemDef3);
+    }
+    $(".list-group-item-action").click(getCityWeather);
 }
 
 function cityInfo(){
+    var cityFound = false;
+    for (let i=0; i<savedSearch.length; i++) {
+        if (cityName.toLowerCase() === savedSearch[i].name.toLowerCase()) {
+            cityFound = true;
+            lastSearch = i;
+            localStorage.setItem(lastSearchName, lastSearch);
+        }
+    }
 
+    if (!cityFound) {
+        var newCityInfo = {};
+        var openWeatherURL = openWeatherEnd + openWeatherCurrent + cityName + APIkey;
+
+        $.ajax({
+            url: openWeatherURL,
+            method: "GET"
+        }).then(function (response) {
+            newCityInfo.name = response.name; 
+            newCityInfo.lat = response.coord.lat; 
+            newCityInfo.lon = response.coord.lon; 
+            lastSearch = savedSearch.push(newCityInfo) - 1; 
+            setsavedSearch(); 
+            searchDisplay();
+            weatherDisplay();
+        }).catch(function (error) {
+            if (error.status == 404) {
+                $("#errorMsg").text('City "' + cityName + '" not found. Please check spelling and try again.');
+            }
+            else {
+                $("#errorMsg").text("Sorry, cannot retrieve weather information. Please try again later.");
+            }
+            $(".modal").modal('show');
+        });
+    }
+    else {
+        weatherDisplay(); 
+    }
 }
 
 function weatherDisplay(){
@@ -52,13 +90,23 @@ function weatherDisplay(){
 
 function doSearch(event){
     event.preventDefault();
-    
+    event.preventDefault();                                     
+    var citySearchInput = $("#city-search-input");
+    var city = citySearchInput.val().trim();
+
+    if (city === "") {
+        $("#errorMsg").text("Please enter a city name.");
+        $(".modal").modal('show');
+    }
+    else {
+        citySearchInput.val("");
+        cityInfo(city);
+    }   
 }
 
-
-
-
-
-
-
-
+function getCityWeather(event){
+    event.preventDefault();
+    lastSearch = $(this).attr("value");
+    localStorage.setItem(lastSearchName, lastSearch);
+    weatherDisplay();
+}
